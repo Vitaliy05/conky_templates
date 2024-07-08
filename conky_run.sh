9ac1021f8bfcb13e4ffc65d320494fe05000b48e
vitasya <vitaliy050793@gmail.com>
2024-07-08 22:31:49 +0200

    deleted some unused configuration files and modified the readme file

--- conky_run.sh
+++ conky_run.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 killall conky
-# conky -c ~/Templates/conky_template/configs/conky_bg &
-# conky -c ~/Templates/conky_template/configs/conky_clock &
-# conky -c ~/Templates/conky_template/configs/conky_txt &
-# conky -p 3 -c ~/Templates/conky_template/configs/conky_files &
 conky -p 6 -c ~/Templates/conky_template/configs/conky_system &
 conky -p 4 -c ~/Templates/conky_template/configs/conky_network &
 conky -p 5 -c ~/Templates/conky_template/configs/conky_cpu &
